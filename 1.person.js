e1d41891cfd30aad992473859d4d80d347a1b031
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-04-13 23:48:32 +0300

    corrected regular expression and added test input

--- 1.person.js
+++ 1.person.js
@@ -1,13 +1,15 @@
-function Person(first, last) {
-    this.firstName = first;
-    this.lastName = last;
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
 
     Object.defineProperty(this, 'fullName', {
+
         get: function () {
             return `${this.firstName} ${this.lastName}`;
         },
+        
         set: function (fullName) {
-            let pattern = /(?<firstName\w+) (?<lastName\w+)/;
+            let pattern = /(?<firstName>\w+) (?<lastName>\w+)/;
             let matchResult = fullName.match(pattern);
 
             if (matchResult) {
@@ -17,3 +19,12 @@ function Person(first, last) {
         }
     });
 }
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
